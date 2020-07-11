87efffcc3169684f85ad49e00c907a2458c5a6a7
saifbashar <saifbashar2021@gmail.com>
2020-07-11 19:42:00 +0600

    Add solution for problem `A - String Task` Link: https://codeforces.com/contest/118/problem/A Tags: implementation, strings, *1000 Ref: https://codeforces.com/contest/118/submission/86507407

--- codeforces/118/A.cpp
+++ codeforces/118/A.cpp
@@ -3,19 +3,43 @@
 #include<ctype.h>
 int main()
 {
-	char s[100];
-	scanf("%s", s);
-	int i;
-	for(i=0;i<strlen(s);i++)
-	{
-		if(tolower(s[i])=='a' || tolower(s[i])=='o' || tolower(s[i])=='y' || tolower(s[i])=='e' || tolower(s[i])=='i' || tolower(s[i])=='u' )
-		{
-		 continue;
-		}
-		else
-		{
-			printf(".%c",tolower(s[i]));
-		}
-	}
-	return 0;
+    char s[100];
+    scanf("%s", s);
+    int i=0,j;
+    j = strlen(s);
+    while(i<j)
+    
+    {
+        if(tolower(s[i]) == 'a')
+        {
+        i++;
+        }
+        else if (tolower(s[i]) == 'e') 
+        {
+           i++;
+        }
+        else if (tolower(s[i]) == 'i') 
+        {
+           i++;
+        }
+        else if (tolower(s[i]) == 'o') 
+        {
+           i++;
+        }
+         else if (tolower(s[i]) == 'u') 
+        {
+           i++;
+        }
+        else if (tolower(s[i]) == 'y')
+        {
+            i++;
+        }
+        
+        else{
+            printf(".%c",tolower(s[i]));
+            i++;
+        }
+       
+    }
+    return 0;
 }
